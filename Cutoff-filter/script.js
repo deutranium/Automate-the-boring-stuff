703941459fe133f76433474febabf4c8245d7103
unknown <29124655+deutranium@users.noreply.github.com>
2019-06-16 00:50:45 +0530

    qazxswedcvfrtgbnhyujmkiolp

--- Cutoff-filter/script.js
+++ Cutoff-filter/script.js
@@ -2,46 +2,35 @@ function threeTwoOneGo(){
 
 	$("tbody").html("<tr><th>Name</th><th>Branch</th><th>Seat Pool</th><th>Opening Rank</th><th>Closing Rank</th></tr>");
 
-	let name = $("input:nth-of-type(1)").val(); // Name of college
-	let branch = $("input:nth-of-type(2)").val().split(","); // Name of branch
-	let branchLength = branch.length;
-	let seatPool = $("input:nth-of-type(3)").val();
-	let rank = 0;	//The default value
-	if($("input:nth-of-type(4)").val() != "") rank = parseInt($("input:nth-of-type(4)").val());	// Value form text field if any
-	let cRank = Infinity;
-	if($("input:nth-of-type(5)").val() != "") cRank = parseInt($("input:nth-of-type(5)").val());	// Value form text field if any
-
-	// * // * // * // * // * // * // * // * |||||||| * \\ * \\ * \\ * \\ * \\ * \\ * \\ * \\
-
+	let name = $("input:nth-of-type(1)").val(); 				// Name of college
+	let branch = $("input:nth-of-type(2)").val().split(","); 	// Name of branch(es)
+	let branchLength = branch.length;							// Number of branches
+	let seatPool = $("input:nth-of-type(3)").val();				// Female only or Gender Neutral
+	let rank = 0;												// The default value
+	if($("input:nth-of-type(4)").val() != "") rank = parseInt($("input:nth-of-type(4)").val());		// Value from text field if any
+	let cRank = Infinity;										// The default balue
+	if($("input:nth-of-type(5)").val() != "") cRank = parseInt($("input:nth-of-type(5)").val());	// Value from text field if any
+	let detailsArr = [];
 
 
 	$.getJSON('round7.json', function(data){	//Get the data as JSON
 
 		let totalPages = data.pageTables.length;
 
-	// data[i][0] = Round Number
-	// data[i][1] = College Name
-	// data[i][2] = Branch
-	// data[i][3] = State Quota n Stuff
-	// data[i][4] = Caste Quota n Stuff
-	// data[i][5] = Female Quota n Stuff
-	// data[i][6] = Opening Rank
-	// data[i][7] = Closing Rank
-
 		for(let i = 0; i < totalPages; i++){	// to loop over the pages
 
 			for(let k = 1; k < data.pageTables[i].tables.length; k++){ // to loop over each row in a page
 				let thisData = data.pageTables[i].tables[k];
-					let branchGood = false;
-					for(let j = 0; j < branchLength; j++){
-						if(thisData[2].toLowerCase().indexOf(branch[j].trim().toLowerCase()) != -1){
-							branchGood = true;
-							//return;
-						}
+				let branchGood = false;
+				for(let j = 0; j < branchLength; j++){
+					if(thisData[2].toLowerCase().indexOf(branch[j].trim().toLowerCase()) != -1){
+						branchGood = true;
 					}
-					if(branchGood && ((thisData[3] == "OS") || (thisData[3] == "AI")) && (thisData[4] == "OPEN") && (thisData[5].toLowerCase().indexOf(seatPool.toLowerCase()) != -1) && (thisData[1].toLowerCase().indexOf(name.toLowerCase()) != -1) && (thisData[7] >= rank) && (thisData[7] <= cRank)){
-						$("tbody").append("<tr><td>" + thisData[1] + "</td><td>" + thisData[2] + "</td><td>" + thisData[5] + "</td><td>" + thisData[6] + "</td><td>" + thisData[7] + "</td></tr>");
-					}
+				}
+				if(branchGood && ((thisData[3] == "OS") || (thisData[3] == "AI")) && (thisData[4] == "OPEN") && (thisData[5].toLowerCase().indexOf(seatPool.toLowerCase()) != -1) && (thisData[1].toLowerCase().indexOf(name.toLowerCase()) != -1) && (thisData[7] >= rank) && (thisData[7] <= cRank)){
+					$("tbody").append("<tr><td>" + thisData[1] + "</td><td>" + thisData[2] + "</td><td>" + thisData[5] + "</td><td>" + thisData[6] + "</td><td>" + thisData[7] + "</td></tr>");
+					detailsArr.push([thisData[1], thisData[2], thisData[5], thisData[6], thisData[7]);
+				}
 			}
 
 		}
@@ -49,6 +38,53 @@ function threeTwoOneGo(){
 
 }
 
+
+
+// thisData[0] = Round Number
+// thisData[1] = College Name
+// thisData[2] = Branch
+// thisData[3] = State Quota n Stuff
+// thisData[4] = Caste Quota n Stuff
+// thisData[5] = Female Quota n Stuff
+// thisData[6] = Opening Rank
+// thisData[7] = Closing Rank
+
+
+
+
+
 function howToUse(){
-	alert("To be used only to filter Round 7(2018) cutoffs given on official JoSAA website. \n \nCollege name : Enter the complete word which occurs at least once in the name of the college. Example : \"IIT\" won't match anything, but \"Indian\" will. \nBranch: Enter the complete word(or words separated by a comma), instead of abbreviations like CS, ECE etc. Example : \"computer\", \"ElEcTrIcAl\", \"Computer  ,     ElEcTriCal\" are valid.\nSeat Pool : Enter \"F\" for female only, \"G\" for Gender Neutral, or leave blank for both. \nIncludes Rank : Matches colleges whose cutoffs allow admission to the student with this rank. \nClosing Rank < \< : The upper limit to the closing ranks\n\nTheir won't be any errors (if any) displayed on the webpage itself in case you're trying to mess up with the criteria. Use Ctrl + Shift + I for Chrome, or <<Insert text here after Googling>> for other browsers to view the console.");
+	alert(`To be used only to filter Round 7(2018) cutoffs given on official JoSAA website.
+
+College name : Enter the complete word which occurs at least once in the name of the college. Example : \"IIT\" won't match anything, but \"Indian\" will.
+Branch: Enter the complete word(or words separated by a comma), instead of abbreviations like CS, ECE etc. Example : \"computer\", \"ElEcTrIcAl\", \"Computer  ,     ElEcTriCal\" are valid.
+Seat Pool : Enter \"F\" for Female only, \"G\" for Gender Neutral, or leave blank for both.
+Includes Rank : Matches colleges whose cutoffs allow admission to the student with this rank.
+Closing Rank < : The upper limit to the closing ranks
+
+Their won't be any errors (if any) displayed on the webpage itself in case you're trying to mess up with the criteria. Use Ctrl + Shift + I for Chrome, or <<Insert text here after Googling>> for other browsers to view the console.`);
+}
+
+
+/*	Trying some node and npm
+function getTable(){
+	let pdf_table_extractor = require('pdf-table-extractor');
+
+	function success(result){
+		console.log(JSON.stringify(result));
+	}
+
+	function error(e){
+		console.error(e);
+	}
+
+	pdf_table_extractor("round7.pdf", success, error);
+}
+
+
+getTable();
+*/
+
+function sortByClosingRanks(){
+
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,6 @@
 # Automate The Boring Stuff
 
-The only reason for the existence of this repo is to save some (if not much) time, and brainpower of lazy species like humans, by automating teeny-weeny but boring tasks which I happen to come across while doing some (not so) random stuff.
-
-P.S. To save some brainpower, I haven't thought much about the UI of any of these (yet). Feel free to improve them.
-
-P.S. again : It's always a work in progress
+[//]: # "The only reason for the existence of this repo is to save some (if not much) time, and brainpower of lazy species like humans, by automating teeny-weeny but boring tasks which I happen to come across while doing some (not so) random stuff.P.S. To save some brainpower, I haven't thought much about the UI of any of these (yet). Feel free to improve them.P.S. again : It's always a work in progress"
 
 - [Escape-all](https://deutranium.github.io/Automate-the-boring-stuff/Escape-all/) : Escape `\`, `` ` ``, `:`, `'`, and `"` from a given text using backslash.
 
